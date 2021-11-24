169c03d152489a4bae62b05d8641c238ffa143ec
Alexandra Weaver <aweaver0124@gmail.com>
2021-11-24 12:02:09 -0600

    commit tree 17f2d3160a46dfb7f48a15fb756d2f040e45e2b0

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob';
+customerName;
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    window.bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+
+const leastFavoriteCustomer = 'karen';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'kevin';
+    return leastFavoriteCustomer;
+}
+changeLeastFavoriteCustomer();
